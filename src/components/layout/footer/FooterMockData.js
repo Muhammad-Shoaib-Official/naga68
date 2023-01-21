0f71713d64080ea8fb033aa78dc0d1e99cc1177e
Muhammad Noman <muhammad.noman@ceative.co.uk>
2023-01-21 12:50:18 +0500

    Comment worked on footer design

---
+++ src/components/layout/footer/FooterMockData.js
@@ -0,0 +1,204 @@
+export const gammingLinks = [
+    {
+      id: 1,
+      name: "All games",
+      linkPath: "/all-games",
+      target: "",
+    },
+    {
+      id: 2,
+      name: "Dice",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 3,
+      name: "Slots",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 4,
+      name: "Rank system",
+      linkPath: "#",
+      target: "_blank",
+    },
+  ];
+  export const FeatueLinks = [
+    {
+      id: 1,
+      name: " Staking",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 2,
+      name: " BetFury Box",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 3,
+      name: "Cashback",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 4,
+      name: "  Daily tasks",
+      linkPath: "#",
+      target: "_blank",
+    },
+  ];
+  export const promoLinks = [
+    {
+      id: 1,
+      name: "Promotions",
+      linkPath: "#",
+      target: "",
+    },
+  ];
+  export const aboutusLinks = [
+    {
+      id: 1,
+      name: "News",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 2,
+      name: " About BFG",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 3,
+      name: " About BetFury team",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 4,
+      name: " BetFury Whitepaper",
+      linkPath: "#",
+      target: "_blank",
+    },
+    {
+      id: 5,
+      name: "  BetFury Docs",
+      linkPath: "#",
+      target: "_blank",
+    },
+    {
+      id: 6,
+      name: "Official Mirrors",
+      linkPath: "#",
+      target: "_blank",
+    },
+  ];
+  export const helpLinks = [
+    {
+      id: 1,
+      name: "Fairness",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 2,
+      name: " Privacy Policy",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 3,
+      name: "  Terms of Service",
+      linkPath: "#",
+      target: "",
+    },
+    {
+      id: 4,
+      name: "Sportsbetting T&C",
+      linkPath: "#",
+      target: "_blank",
+    },
+    {
+      id: 5,
+      name: " Bug Bounty Program",
+      linkPath: "#",
+      target: "_blank",
+    },
+    {
+      id: 6,
+      name: "  Live Support",
+      linkPath: "#",
+      target: "_blank",
+    },
+    {
+      id: 7,
+      name: " Responsible Gambling",
+      linkPath: "#",
+      target: "_blank",
+    },
+  ];
+  export const socialIcon = [
+    {
+      id: 1,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/teligram.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 2,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/twitter.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 3,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/instagram.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 4,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/youtube.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 5,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/setmeet.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 6,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/bitcointalk.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 7,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/tikto.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 8,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/pentirest.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 9,
+      linkPath: "#",
+      imgPath: "/assets/footerimges/socialicon/discord.svg",
+      alttext: "icon Img",
+    },
+    {
+      id: 10,
+      linkPath: "#",
+      imgPath: '/assets/footerimges/socialicon/reddit.svg',
+      alttext: "icon Img",
+    },
+  ];
--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -4,7 +4,8 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import img from "../../../../public/assets/footerimges/f1.png";
 import img2 from "../../../../public/assets/footerimges/f2.png";
-import teligram from "../../../../public/assets/footerimges/socialicon/teligram.svg";
+import {gammingLinks,FeatueLinks,promoLinks,aboutusLinks,helpLinks,socialIcon} from "./FooterMockData"
+// import teligram from "../../../../public/assets/footerimges/socialicon/teligram.svg";
 import FooterSlider from "./footerSilder";
 const Footer = () => {
   const [windowWidth, setWindowWidth] = useState(
@@ -23,210 +24,6 @@ const Footer = () => {
   const handleMobilClick = () => {
     alert("heloo");
   };
-  const gammingLinks = [
-    {
-      id: 1,
-      name: "All games",
-      linkPath: "/all-games",
-      target: "",
-    },
-    {
-      id: 2,
-      name: "Dice",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 3,
-      name: "Slots",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 4,
-      name: "Rank system",
-      linkPath: "#",
-      target: "_blank",
-    },
-  ];
-  const FeatueLinks = [
-    {
-      id: 1,
-      name: " Staking",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 2,
-      name: " BetFury Box",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 3,
-      name: "Cashback",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 4,
-      name: "  Daily tasks",
-      linkPath: "#",
-      target: "_blank",
-    },
-  ];
-  const promoLinks = [
-    {
-      id: 1,
-      name: "Promotions",
-      linkPath: "#",
-      target: "",
-    },
-  ];
-  const aboutusLinks = [
-    {
-      id: 1,
-      name: "News",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 2,
-      name: " About BFG",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 3,
-      name: " About BetFury team",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 4,
-      name: " BetFury Whitepaper",
-      linkPath: "#",
-      target: "_blank",
-    },
-    {
-      id: 5,
-      name: "  BetFury Docs",
-      linkPath: "#",
-      target: "_blank",
-    },
-    {
-      id: 6,
-      name: "Official Mirrors",
-      linkPath: "#",
-      target: "_blank",
-    },
-  ];
-  const helpLinks = [
-    {
-      id: 1,
-      name: "Fairness",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 2,
-      name: " Privacy Policy",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 3,
-      name: "  Terms of Service",
-      linkPath: "#",
-      target: "",
-    },
-    {
-      id: 4,
-      name: "Sportsbetting T&C",
-      linkPath: "#",
-      target: "_blank",
-    },
-    {
-      id: 5,
-      name: " Bug Bounty Program",
-      linkPath: "#",
-      target: "_blank",
-    },
-    {
-      id: 6,
-      name: "  Live Support",
-      linkPath: "#",
-      target: "_blank",
-    },
-    {
-      id: 7,
-      name: " Responsible Gambling",
-      linkPath: "#",
-      target: "_blank",
-    },
-  ];
-  const socialIcon = [
-    {
-      id: 1,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/teligram.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 2,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/twitter.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 3,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/instagram.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 4,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/youtube.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 5,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/setmeet.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 6,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/bitcointalk.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 7,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/tikto.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 8,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/pentirest.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 9,
-      linkPath: "#",
-      imgPath: "/assets/footerimges/socialicon/discord.svg",
-      alttext: "icon Img",
-    },
-    {
-      id: 10,
-      linkPath: "#",
-      imgPath: '/assets/footerimges/socialicon/reddit.svg',
-      alttext: "icon Img",
-    },
-  ];
   return (
     <footer className="footer_main">
       {/* <h2>Width: {windowWidth}</h2> */}
@@ -390,12 +187,15 @@ const Footer = () => {
               </Link>
             ))}
           </div>
+          <div className="coin_detail_wrapper">
+            <div className="first_one">
+              heloo
+            </div>
+          </div>
         </div>
         <Divider className="divider" />
         <div className="copy_write_main">
           <p className="Copywrite_text">Copyright © 2023 NAGA. All rights reserved.</p>
-          
-
         </div>
       </div>
     </footer>
