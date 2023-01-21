106a348eef12baff1b8f9ffc033caec7b313af83
Muhammad Shoaib <muhammad.shaoib@ceative.co.uk>
2023-01-21 22:56:37 +0500

    fexes in footer

--- src/components/layout/footer/FooterSilder.js
+++ src/components/layout/footer/FooterSilder.js
@@ -4,42 +4,42 @@ import Slider from "react-slick";
 import img from "../../../../public/assets/footerimges/sliderimges/01.png";
 
 const SimpleSlider = () => {
-  var settings = {
-    dots: false,
-    speed: 500,
-    slidesToShow: 7,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    infinite: true,
-    autoplay: true,
-    arrow: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  // var settings = {
+  //   dots: false,
+  //   speed: 500,
+  //   slidesToShow: 7,
+  //   slidesToScroll: 1,
+  //   initialSlide: 0,
+  //   infinite: true,
+  //   autoplay: true,
+  //   arrow: false,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
   return (
     <div className="silder_mainn">
       {/* <Slider {...settings}>
